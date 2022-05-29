4fd087647b711bdf3bd39c5d46823242e154c96b
Taegyeong Lee <hanarotg@naver.com>
2022-05-30 05:05:18 +0900

    [add] 기초 디자인 및 API 연결

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const axios = require("axios");
+
+require("dotenv").config();
+
+const app = express();
+const port = 8000;
+
+app.use("/static", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+// 체크보드 페이지
+app.get("/cb", async (req, res) => {
+  const id = encodeURI(req.query.id);
+  const config = {
+    headers: {
+      "X-Riot-Token": `${process.env.API_KEY}`,
+    },
+  };
+
+  try {
+    // 개선사항 : 병렬처리하기 두 axios 는
+    const summoner = await axios.get(
+      `${process.env.LOL_SUMMONER_URL}/${id}`,
+      config
+    );
+
+    const idEncrypted = summoner.data.id;
+
+    const spectator = await axios.get(
+      `${process.env.LOL_SPECTATOR_URL}/${idEncrypted}`,
+      config
+    );
+
+    console.log(spectator.data);
+    res.render("checkboard", { id: decodeURI(id), data: spectator.data });
+  } catch (error) {
+    res.render("checkboard-error", {
+      id: decodeURI(id),
+    });
+  }
+});
+
+// index
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port);
